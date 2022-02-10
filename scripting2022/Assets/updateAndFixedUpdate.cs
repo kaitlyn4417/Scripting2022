ced8e40394dacf8934bd82e3d5abb36502c3e5e0
kaitlyng <10957070@uvu.edu>
2022-02-10 16:51:40 -0700

    added updateAndFixedUpdate

---
+++ scripting2022/Assets/updateAndFixedUpdate.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class updateAndFixedUpdate : MonoBehaviour
+{
+    void FixedUpdate ()
+    {
+        Debug.Log("FixedUpdate time :" + Time.deltaTime);
+    }
+    void Update ()
+    {
+        Debug.Log("Update time :" + Time.deltaTime);
+    }
+}
