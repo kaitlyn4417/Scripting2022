2da564afe59db0fe87f32cde70e44666060fefd1
kaitlyng <10957070@uvu.edu>
2022-02-10 18:22:40 -0700

    added invoke

---
+++ scripting2022/Assets/invoke.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class invoke : MonoBehaviour
+{
+    public GameObject target;
+    void Start()
+    {
+        Invoke ("SpawnObject", 2);
+    }
+    void SpawnObject()
+    {
+        Instantiate(target, new Vector3(0, 2, 0), Quaternion.identity);
+    }
+}
