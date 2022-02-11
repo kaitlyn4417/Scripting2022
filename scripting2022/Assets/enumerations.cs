8ec430263b4b7e78a8f01d83b0fc6b9592adb6b3
kaitlyng <10957070@uvu.edu>
2022-02-10 18:27:10 -0700

    added enumerations

---
+++ scripting2022/Assets/enumerations.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enumerations : MonoBehaviour
+{
+    enum Direction {North, East, South, West};
+    void Start () 
+    {
+        Direction myDirection;
+        
+        myDirection = Direction.North;
+    }
+    Direction ReverseDirection (Direction dir)
+    {
+        if(dir == Direction.North)
+            dir = Direction.South;
+        else if(dir == Direction.South)
+            dir = Direction.North;
+        else if(dir == Direction.East)
+            dir = Direction.West;
+        else if(dir == Direction.West)
+            dir = Direction.East;
+        return dir;     
+    }
+}
