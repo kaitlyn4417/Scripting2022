5820a00fd3164944c9f83804f8698d5966cd2eb1
kaitlyng <10957070@uvu.edu>
2022-02-10 18:12:00 -0700

    added arrays

---
+++ scripting2022/Assets/arrays.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class arrays : MonoBehaviour
+{
+    public GameObject[] players;
+    void Start ()
+    {
+        players = GameObject.FindGameObjectsWithTag("Player");
+        for(int i = 0; i < players.Length; i++)
+        {
+            Debug.Log("Player Number "+i+" is named "+players[i].name);
+        }
+    }
+}
