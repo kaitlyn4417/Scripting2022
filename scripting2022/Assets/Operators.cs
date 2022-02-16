603a9ed11c14d92c3855424afd6ac5df4557beae
kaitlyng <10957070@uvu.edu>
2022-02-16 15:39:06 -0700

    added Operators.cs

---
+++ scripting2022/Assets/Operators.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class C : MonoBehaviour
+{
+
+    void Start()
+    {
+        int x = 243 + 167;
+        x += 49;
+
+        Console.WriteLine(x);
+    }
+    
+    void Update()
+    {
+        
+    }
+}
