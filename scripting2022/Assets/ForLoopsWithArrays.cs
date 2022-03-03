9407c7fb67119a28a02f8f100d870f891166edfb
kaitlyng <10957070@uvu.edu>
2022-03-03 11:20:24 -0700

    added ForLoopsWithArrays.cs

---
+++ scripting2022/Assets/ForLoopsWithArrays.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ForLoopsWithArrays : MonoBehaviour
+{
+    void Start()
+    {
+        string[] veggies = {"Carrots", "Cucumbers", "Green Peppers", "Broccoli"};
+        
+        for(int i = 0, i < veggies.Length, i++)
+        {
+            Console.WriteLine(veggies[i]);
+        }
+    }
+
+    void Update()
+    {
+        
+    }
+}
