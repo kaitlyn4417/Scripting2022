06f351ed47c212ed3630c4b809c8c514147b94ca
kaitlyng <10957070@uvu.edu>
2022-03-03 11:12:50 -0700

    added whileLoops.cs

---
+++ scripting2022/Assets/whileLoops.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class whileLoops : MonoBehaviour
+{
+
+    void Start()
+    {
+        int i = 0;
+		while (i < 10)
+		{
+			Console.WriteLine(i);
+			i++:
+		}
+
+    }
+
+    void Update()
+    {
+        
+    }
+}
