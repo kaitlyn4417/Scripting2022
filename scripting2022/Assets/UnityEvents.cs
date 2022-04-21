f619ab0322ee21d0fbd92ecff56e9abf5506d378
kaitlyng <10957070@uvu.edu>
2022-04-20 19:11:26 -0600

    added UnityEvents

---
+++ scripting2022/Assets/UnityEvents.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+	public UnityEvent awakeEvent, startEvent, onEnableEvent;
+	public float holdTime;
+
+	private void Awake()
+	{
+		awakeEvent.Invoke();
+	} 
+
+    // Start is called before the first frame update
+    void IEnumerator Start()
+    {
+        yield return new WaitForSeconds(holdTime);
+		startEvent.Invoke();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+	private void onEnable()
+	{
+		onEnable.Invoke();
+	}
+}
